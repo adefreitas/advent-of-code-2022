e3afe20dd108c0323916b83e7ec0f180e2c97c0a
adefreitas <6485562+adefreitas@users.noreply.github.com>
2022-12-03 18:48:01 +0100

    day 3: part 1

---
+++ day-3/src/main.rs
@@ -0,0 +1,81 @@
+/*
+
+One Elf has the important job of loading all of the rucksacks with supplies for the jungle journey. Unfortunately, that Elf didn't quite follow the packing instructions, and so a few items now need to be rearranged.
+
+Each rucksack has two large compartments. All items of a given type are meant to go into exactly one of the two compartments. The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.
+
+The Elves have made a list of all of the items currently in each rucksack (your puzzle input), but they need your help finding the errors. Every item type is identified by a single lowercase or uppercase letter (that is, a and A refer to different types of items).
+
+The list of items for each rucksack is given as characters all on a single line. A given rucksack always has the same number of items in each of its two compartments, so the first half of the characters represent items in the first compartment, while the second half of the characters represent items in the second compartment.
+
+For example, suppose you have the following list of contents from six rucksacks:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+The first rucksack contains the items vJrwpWtwJgWrhcsFMMfFFhFp, which means its first compartment contains the items vJrwpWtwJgWr, while the second compartment contains the items hcsFMMfFFhFp. The only item type that appears in both compartments is lowercase p.
+The second rucksack's compartments contain jqHRNqRjqzjGDLGL and rsFMfFZSrLrFZsSL. The only item type that appears in both compartments is uppercase L.
+The third rucksack's compartments contain PmmdzqPrV and vPwwTWBwg; the only common item type is uppercase P.
+The fourth rucksack's compartments only share item type v.
+The fifth rucksack's compartments only share item type t.
+The sixth rucksack's compartments only share item type s.
+
+To help prioritize item rearrangement, every item type can be converted to a priority:
+
+    Lowercase item types a through z have priorities 1 through 26.
+    Uppercase item types A through Z have priorities 27 through 52.
+
+In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
+
+Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+*/
+
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("../input.txt").expect("Input read");
+    // Process the input
+    let total_prio = contents.split("\n").fold(0, |acc, backpack| -> u32 {
+        let mut repeated_item_index: usize = 0;
+        let mut i = 0;
+        let mut j = backpack.chars().count() / 2;
+        println!("Length is {:?}, half point is {:?}", backpack.chars().count(), backpack.chars().count() / 2);
+        // Find the repeated char
+        while i != j {
+            if backpack.chars().nth(i).unwrap() == backpack.chars().nth(j).unwrap() {
+                repeated_item_index = i;
+                break;
+            }
+            j += 1;
+            if j == backpack.chars().count() {
+                j = backpack.chars().count() / 2;
+                i += 1;
+            }
+        }
+        // Get the ascii value of the char for some magic
+        let ascii_value = backpack.chars().nth(repeated_item_index).unwrap() as u32;
+
+        // Substract the value needed to normalise the value
+        let priority: u32 = if ascii_value >= 65 && ascii_value <= 90 {
+            ascii_value - 38 
+        } else {
+           ascii_value - 96
+        };
+
+        println!(
+            "repeated item is {:?} and ascii value is {:?} and priority value is {:?}, accumulator has {acc} for {backpack}",
+            backpack.chars().nth(repeated_item_index).unwrap(),
+            ascii_value,
+            priority
+
+        );
+        return acc + priority;
+    });
+
+
+    println!("Total priority of items is {total_prio:?}");
+}
