84624cdd9dd9c45eeb0ec170a5f01ead6fd233b7
adefreitas <6485562+adefreitas@users.noreply.github.com>
2022-12-09 15:07:53 +0100

    day 7: part 1

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Borrow, cell::RefCell, fs, rc::Rc};
+use std::{borrow::Borrow, cell::RefCell, fs, ops::AddAssign, rc::Rc};
 
 struct Node<'a> {
     pub size: Option<u32>,
@@ -31,18 +31,18 @@ fn main() {
                 })
                 .filter(|line| !line.is_empty())
                 .collect();
-            println!("Command {:?}", command);
+            // println!("Command {:?}", command);
             if command[0][0] == "cd" {
                 if command[0][1] == "/" {
                     current_node = Rc::clone(&root_node);
-                    println!("Going to root node");
+                    // println!("Going to root node");
                 } else if command[0][1] == ".." {
-                    println!("Going up one level");
+                    // println!("Going up one level");
                     let parent =
                         Rc::clone(&current_node.as_ref().borrow().parent.as_ref().unwrap());
                     current_node = parent;
                 } else {
-                    println!("Going to node {:?}", command[0][1]);
+                    // println!("Going to node {:?}", command[0][1]);
                     let next = Rc::clone(
                         current_node
                             .as_ref()
@@ -59,11 +59,11 @@ fn main() {
             }
 
             if command[0][0] == "ls" {
-                println!("Listing elements in directory {:?}", command);
+                // println!("Listing elements in directory {:?}", command);
                 for index in 1..command.len() {
-                    println!("Command {:?}", command[index]);
+                    // println!("Command {:?}", command[index]);
                     if command[index][0] == "dir" {
-                        println!("{:?} is a dir, creating node", command[index][1]);
+                        // println!("{:?} is a dir, creating node", command[index][1]);
                         let node = Rc::new(RefCell::new(Node {
                             name: command[index][1],
                             size: None,
@@ -73,10 +73,10 @@ fn main() {
                         current_node.as_ref().borrow_mut().children.push(node);
                         // println!("Node {:?}", node.as_ref().borrow_mut().name);
                     } else {
-                        println!(
-                            "{:?} is a file, creating node with no children",
-                            command[index][1]
-                        );
+                        // println!(
+                        //     "{:?} is a file, creating node with no children",
+                        //     command[index][1]
+                        // );
                         let node = Rc::new(RefCell::new(Node {
                             name: command[index][1],
                             size: Option::Some(command[index][0].parse::<u32>().unwrap()),
@@ -89,11 +89,16 @@ fn main() {
                 }
             }
         });
-    let total_size = get_total_size(&root_node);
-    println!("Total size {:?}", total_size);
+    let total_with_condition: Rc<RefCell<u32>> = Rc::new(RefCell::new(0));
+    let total_size = get_total_size(&root_node, &total_with_condition);
+    println!(
+        "Total size {:?}, with condition is {:?}",
+        total_size,
+        total_with_condition.as_ref().borrow_mut()
+    );
 }
 
-fn get_total_size(node: &Rc<RefCell<Node>>) -> u32 {
+fn get_total_size(node: &Rc<RefCell<Node>>, total_with_condition: &Rc<RefCell<u32>>) -> u32 {
     let cloned_node = Rc::clone(node);
     let borrowed_mut = cloned_node.as_ref().borrow_mut();
     let size = borrowed_mut.size.unwrap_or_default();
@@ -103,13 +108,19 @@ fn get_total_size(node: &Rc<RefCell<Node>>) -> u32 {
     }
 
     let dir_size = borrowed_mut.children.iter().fold(0, |acc, inner_dir| {
-        return acc + get_total_size(inner_dir);
+        return acc + get_total_size(inner_dir, total_with_condition);
     });
     if borrowed_mut.size.borrow().is_none() {
         println!(
             "Directory {:?} is of size {:?}",
             borrowed_mut.name, dir_size
         );
+    }
+    if dir_size <= 100000 {
+        total_with_condition
+            .as_ref()
+            .borrow_mut()
+            .add_assign(dir_size);
     }
     return dir_size;
 }
